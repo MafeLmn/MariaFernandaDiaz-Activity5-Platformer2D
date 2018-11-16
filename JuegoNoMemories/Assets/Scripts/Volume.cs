32d1d581ad9452e9ffc18218f93baca7f6a42da0
PAULA M. CASTRO <42016035+PAULAMC1@users.noreply.github.com>
2018-11-16 06:06:04 -0500

    Nuevo

---
+++ JuegoNoMemories/Assets/Scripts/Volume.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class Volume : MonoBehaviour{
+
+    public AudioMixer masterMixer;
+
+    public void SetSFXLvl (float SFXlevel)
+
+    {
+        masterMixer.SetFloat ("SFXVolume", SFXlevel);
+    }
+
+public void SetMusiclevel (float musiclevel)
+{
+    masterMixer.SetFloat("MusicVolume", musiclevel);
+}
+
+}
+
